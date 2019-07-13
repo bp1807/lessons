d1b0625553313b25e93f01cec6d98187acf01b23
bp1807 <36404250+bp1807@users.noreply.github.com>
2019-07-14 00:47:07 +0530

    Add files via upload

---
+++ class-lessons/src/main/java/com/abc/cricket/model/staticdata/Player.java
@@ -0,0 +1,36 @@
+/**
+ * 
+ */
+package com.abc.cricket.model.staticdata;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+/**
+ * @author Soham.Chakravarti
+ *
+ */
+@Entity
+@Table(name="PLAYER")
+@Getter @Setter @ToString
+public class Player {
+	
+	@Id
+	@SequenceGenerator(name="SEQ_PLAYER")
+	@Column(name="ID")
+	private int id;
+	
+	@Column(name="FIRST_NAME")
+	private String firstName;
+	
+	@Column(name="LAST_NAME")
+	private String lastName;
+	
+}
